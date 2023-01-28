264eb2aa76bb0e104394ed4dd0832039ab68add6
Colwaer <674329253@qq.com>
2023-01-28 14:20:01 +0800

    PCSS & Z Pre Pass

---
+++ Code/Shadows/Shaders/Common.hlsl
@@ -0,0 +1,236 @@
+//***************************************************************************************
+// Common.hlsl by Frank Luna (C) 2015 All Rights Reserved.
+//***************************************************************************************
+
+// Defaults for number of lights.
+#ifndef NUM_DIR_LIGHTS
+    #define NUM_DIR_LIGHTS 3
+#endif
+
+#ifndef NUM_POINT_LIGHTS
+    #define NUM_POINT_LIGHTS 0
+#endif
+
+#ifndef NUM_SPOT_LIGHTS
+    #define NUM_SPOT_LIGHTS 0
+#endif
+
+// Include structures and functions for lighting.
+#include "LightingUtil.hlsl"
+
+#define N_SAMPLE 16
+static float2 poissonDisk[16] =
+{
+    float2(-0.94201624, -0.39906216),
+    float2(0.94558609, -0.76890725),
+    float2(-0.094184101, -0.92938870),
+    float2(0.34495938, 0.29387760),
+    float2(-0.91588581, 0.45771432),
+    float2(-0.81544232, -0.87912464),
+    float2(-0.38277543, 0.27676845),
+    float2(0.97484398, 0.75648379),
+    float2(0.44323325, -0.97511554),
+    float2(0.53742981, -0.47373420),
+    float2(-0.26496911, -0.41893023),
+    float2(0.79197514, 0.19090188),
+    float2(-0.24188840, 0.99706507),
+    float2(-0.81409955, 0.91437590),
+    float2(0.19984126, 0.78641367),
+    float2(0.14383161, -0.14100790)
+};
+
+struct MaterialData
+{
+	float4   DiffuseAlbedo;
+	float3   FresnelR0;
+	float    Roughness;
+	float4x4 MatTransform;
+	uint     DiffuseMapIndex;
+	uint     NormalMapIndex;
+	uint     MatPad1;
+	uint     MatPad2;
+};
+
+TextureCube gCubeMap : register(t0);
+Texture2D gShadowMap : register(t1);
+
+// An array of textures, which is only supported in shader model 5.1+.  Unlike Texture2DArray, the textures
+// in this array can be different sizes and formats, making it more flexible than texture arrays.
+Texture2D gTextureMaps[10] : register(t2);
+
+// Put in space1, so the texture array does not overlap with these resources.  
+// The texture array will occupy registers t0, t1, ..., t3 in space0. 
+StructuredBuffer<MaterialData> gMaterialData : register(t0, space1);
+
+
+SamplerState gsamPointWrap        : register(s0);
+SamplerState gsamPointClamp       : register(s1);
+SamplerState gsamLinearWrap       : register(s2);
+SamplerState gsamLinearClamp      : register(s3);
+SamplerState gsamAnisotropicWrap  : register(s4);
+SamplerState gsamAnisotropicClamp : register(s5);
+SamplerComparisonState gsamShadow : register(s6);
+
+// Constant data that varies per frame.
+cbuffer cbPerObject : register(b0)
+{
+    float4x4 gWorld;
+	float4x4 gTexTransform;
+	uint gMaterialIndex;
+	uint gObjPad0;
+	uint gObjPad1;
+	uint gObjPad2;
+};
+
+cbuffer cbPass : register(b1)
+{
+    float4x4 gView;
+    float4x4 gInvView;
+    float4x4 gProj;
+    float4x4 gInvProj;
+    float4x4 gViewProj;
+    float4x4 gInvViewProj;
+    float4x4 gShadowTransform;
+    float3 gEyePosW;
+    float cbPerObjectPad1;
+    float2 gRenderTargetSize;
+    float2 gInvRenderTargetSize;
+    float gNearZ;
+    float gFarZ;
+    float gTotalTime;
+    float gDeltaTime;
+    float4 gAmbientLight;
+
+    // Indices [0, NUM_DIR_LIGHTS) are directional lights;
+    // indices [NUM_DIR_LIGHTS, NUM_DIR_LIGHTS+NUM_POINT_LIGHTS) are point lights;
+    // indices [NUM_DIR_LIGHTS+NUM_POINT_LIGHTS, NUM_DIR_LIGHTS+NUM_POINT_LIGHT+NUM_SPOT_LIGHTS)
+    // are spot lights for a maximum of MaxLights per object.
+    Light gLights[MaxLights];
+};
+
+//---------------------------------------------------------------------------------------
+// Transforms a normal map sample to world space.
+//---------------------------------------------------------------------------------------
+float3 NormalSampleToWorldSpace(float3 normalMapSample, float3 unitNormalW, float3 tangentW)
+{
+	// Uncompress each component from [0,1] to [-1,1].
+	float3 normalT = 2.0f*normalMapSample - 1.0f;
+
+	// Build orthonormal basis.
+	float3 N = unitNormalW;
+	float3 T = normalize(tangentW - dot(tangentW, N)*N);
+	float3 B = cross(N, T);
+
+	float3x3 TBN = float3x3(T, B, N);
+
+	// Transform from tangent space to world space.
+	float3 bumpedNormalW = mul(normalT, TBN);
+
+	return bumpedNormalW;
+}
+
+//---------------------------------------------------------------------------------------
+// PCF for shadow mapping.
+//---------------------------------------------------------------------------------------
+
+float CalcShadowFactor(float4 shadowPosH)
+{
+    // Complete projection by doing division by w.
+    shadowPosH.xyz /= shadowPosH.w;
+
+    // Depth in NDC space.
+    float depth = shadowPosH.z;
+
+    uint width, height, numMips;
+    gShadowMap.GetDimensions(0, width, height, numMips);
+
+    // Texel size.
+    float dx = 1.0f / (float)width;
+
+    float percentLit = 0.0f;
+    const float2 offsets[9] =
+    {
+        float2(-dx,  -dx), float2(0.0f,  -dx), float2(dx,  -dx),
+        float2(-dx, 0.0f), float2(0.0f, 0.0f), float2(dx, 0.0f),
+        float2(-dx,  +dx), float2(0.0f,  +dx), float2(dx,  +dx)
+    };
+
+    [unroll]
+    for(int i = 0; i < 9; ++i)
+    {
+        percentLit += gShadowMap.SampleCmpLevelZero(gsamShadow,
+            shadowPosH.xy + offsets[i], depth).r;
+    }
+    
+    return percentLit / 9.0f;
+}
+// PCSS
+float CalcBlockerDepth(float4 shadowPosH, float width, float lightSize)
+{
+    float sum = 0.f;
+    float count = 0.0005f;
+    
+    shadowPosH.xyz /= shadowPosH.w;
+
+    // Depth in NDC space.
+    float depth = shadowPosH.z;
+    float step = 3;
+    float searchWidth = lightSize / (float) width;
+    for (int i = 0; i < N_SAMPLE; i++)
+    {
+        float2 unitSampleOffset = poissonDisk[i];
+        float2 offset = unitSampleOffset * searchWidth;
+        float2 uvo = shadowPosH.xy + offset;
+        float sampleRes = gShadowMap.Sample(gsamLinearWrap, uvo).r;
+        if (sampleRes < depth)
+        {
+            count += 1;
+            sum += sampleRes;
+        }
+    }
+    return sum / count;
+}
+float PCSS(float4 shadowPosH)
+{
+    // Complete projection by doing division by w.
+    shadowPosH.xyz /= shadowPosH.w;
+
+    // Depth in NDC space.
+    float depth = shadowPosH.z;
+    float lightSize = 15.f;
+    uint width, height, numMips;
+    gShadowMap.GetDimensions(0, width, height, numMips);
+    float orthoDistance = gFarZ - gNearZ;
+
+    float blockerDepth = CalcBlockerDepth(shadowPosH, width, lightSize);
+    float d_blocker = blockerDepth;
+    float d_receiver = depth;
+    float w = (d_receiver - d_blocker) * lightSize / d_blocker;
+    float filterRadius = w / (float)width;
+    float percentLit = 0.0f;
+    float step = 3;
+    
+
+    float ddxu = ddx(shadowPosH.x);
+    float ddxv = ddx(shadowPosH.y);
+    float ddyu = ddy(shadowPosH.x);
+    float ddyv = ddy(shadowPosH.y);
+    float ddxz = ddx(shadowPosH.z);
+    float ddyz = ddx(shadowPosH.z);
+    
+    float2x2 ddxuvMatrics = { ddyv, -ddxv, -ddyu, ddxu };
+    float ddxuvMul = 1 / (ddxu * ddyv - ddxv * ddyu);
+    for (int i = 0; i < N_SAMPLE; i++)
+    {
+        float2 unitSampleOffset = poissonDisk[i];
+        float2 offset = unitSampleOffset * filterRadius;
+        float2 deltaXY = mul(ddxuvMatrics, offset * ddxuvMul);
+        float deltaDepth = deltaXY.x * ddxz + deltaXY * ddyz;
+        percentLit += gShadowMap.SampleCmpLevelZero(gsamShadow,
+        shadowPosH.xy + offset, depth + deltaDepth).r;
+    }
+    
+    return percentLit / (N_SAMPLE);
+
+}
+
---
+++ Code/Shadows/Shaders/DepthOnly.hlsl
@@ -0,0 +1,60 @@
+// Include common HLSL code.
+#include "Common.hlsl"
+
+struct VertexIn
+{
+    float3 PosL : POSITION;
+    float3 NormalL : NORMAL;
+    float2 TexC : TEXCOORD;
+    float3 TangentU : TANGENT;
+};
+
+struct VertexOut
+{
+    float4 PosH : SV_POSITION;
+    float4 ShadowPosH : POSITION0;
+    float3 PosW : POSITION1;
+    float3 NormalW : NORMAL;
+    float3 TangentW : TANGENT;
+    float2 TexC : TEXCOORD;
+};
+
+
+VertexOut VS(VertexIn vin)
+{
+    VertexOut vout = (VertexOut) 0.0f;
+
+	// Fetch the material data.
+    MaterialData matData = gMaterialData[gMaterialIndex];
+	
+    // Transform to world space.
+    float4 posW = mul(float4(vin.PosL, 1.0f), gWorld);
+    vout.PosW = posW.xyz;
+
+    // Transform to homogeneous clip space.
+    vout.PosH = mul(posW, gViewProj);
+	
+	// Output vertex attributes for interpolation across triangle.
+    float4 texC = mul(float4(vin.TexC, 0.0f, 1.0f), gTexTransform);
+    vout.TexC = mul(texC, matData.MatTransform).xy;
+
+    return vout;
+}
+
+void PS(VertexOut pin)
+{
+    // Fetch the material data.
+    MaterialData matData = gMaterialData[gMaterialIndex];
+    float4 diffuseAlbedo = matData.DiffuseAlbedo;
+    uint diffuseMapIndex = matData.DiffuseMapIndex;
+	
+    // Dynamically look up the texture in the array.
+    diffuseAlbedo *= gTextureMaps[diffuseMapIndex].Sample(gsamAnisotropicWrap, pin.TexC);
+
+#ifdef ALPHA_TEST
+    // Discard pixel if texture alpha < 0.1.  We do this test as soon 
+    // as possible in the shader so that we can potentially exit the
+    // shader early, thereby skipping the rest of the shader code.
+    clip(diffuseAlbedo.a - 0.1f);
+#endif
+}
